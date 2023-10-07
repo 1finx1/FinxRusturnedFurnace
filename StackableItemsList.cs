24a59f8cc3f4ab326a046aa9a69895837e759fdb
1finx1 <141705301+1finx1@users.noreply.github.com>
2023-10-07 07:07:42 +0100

    Create StackableItemsList.cs

---
+++ StackableItemsList.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+
+namespace FinxFurnace.Types
+{
+    public class StackingItemConversion
+    {
+        public ushort ID { get; set; } // Input item ID
+        public ushort ID10x { get; set; } // Conversion to 10x stack
+        public ushort ID50x { get; set; } // Conversion to 50x stack
+        public ushort ID100x { get; set; } // Conversion to 100x stack
+        public ushort ID300x { get; set; } // Conversion to 300x stack
+    }
+}
+
+
+NOT FUNCTIONAL CURRENTLY STOPPED DEVELOPING AND HAVNT FINISHED THIS
